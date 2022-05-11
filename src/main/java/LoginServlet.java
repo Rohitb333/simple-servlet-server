9e037ec5151b61b0ddc1bfdbcb5ae1612e517865
Rohit <rohitb333@gmail.com>
2022-05-11 20:47:46 +0530

    Added: Logic to create simple servelet server

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,43 @@
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(
+        description="Login Servlet testing",
+        urlPatterns={"/LoginServlet"}
+        /*initParams = {
+                @WebInitParam(name = "user",value = "rohit"),
+                @WebInitParam(name = "pass",value = "bansod")
+                }*/
+) /*(name = "LoginServlet", value = "/LoginServlet")*/
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user= request.getParameter("user");
+        String pass= request.getParameter("pass");
+
+        //String userId = getServletConfig().getInitParameter("user");
+        //String password= getServletConfig().getInitParameter("pass");
+
+        Pattern namePattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        Matcher nameMatcher = namePattern.matcher(user);
+
+        Pattern passPattern = Pattern.compile("^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$");
+        Matcher passMatcher = passPattern.matcher(pass);
+        if (nameMatcher.find() && passMatcher.find()){
+            request.setAttribute("user",user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
+        }else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>enter user id and password wrong.</font>");
+            rd.include(request,response);
+        }
+    }
+}
